f3730c4c78a98ca1ebf79e93e9abb4f47dee8ce3
ajays1475 <ajays1475@gmail.com>
2018-04-29 01:57:58 +0530

    implemented gulp

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ var express = require('express');
 
 var app = express();
 
-var port = '8080'
+var port = '8080';
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function (req, res) {
     res.send('Hello World');
-})
+});
 
 app.get('/books', function (req, res) {
     res.send('Hello Books');
-})
+});
 
 app.listen(port, function (err) {
     console.log('Server listening on port ' + port);
-})
+});
